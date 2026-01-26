cb64d73562a22840f8fe02360f09114061d6cd23
Ezequiel Lino <sonisombra@gmail.com>
2026-01-25 22:34:00 -0600

    mostrar listados de elementos

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,24 +1,29 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: 'Nintendo Switch', quantity: 1 },
+  { productName: 'Pro Controller', quantity: 2 },
+  { productName: 'Super Smash', quantity: 5 },
+]
+
 export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de compras</h1>
 
-      <ItemCounter
-        name='Nintendo Switch'
-        quantity={10}
-      />
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter
+          key={productName}
+          name={productName}
+          quantity={quantity}
+        />
+      ))}
 
-      <ItemCounter
-        name='Pro controller'
-        quantity={1}
-      />
-
-      <ItemCounter
-        name='Nintendo Switch'
-        quantity={2}
-      />
     </>
   )
 }
